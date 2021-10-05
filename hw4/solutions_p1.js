a00714c4c06040b3b83bbfe3ed39796f529a361d
Sasha Koval <31686918+sashakoval@users.noreply.github.com>
2021-10-05 19:46:37 +0300

    fixed

--- hw4/solutions_p1.js
+++ hw4/solutions_p1.js
@@ -11,12 +11,17 @@ let toCamelCase = (arr) => {
 
 // Створити функцію яка отримує параметром масив цілих чисел 1 <= value <= 26, і повертала б масив у якому кожен елемент масиву є літерою англійського алфавіту
 // відповідний конкретному елементу масива (наприклад 1 = a, 2 = b, 3 = c, 4 = d, …). Приклад [4, 3, 22, 11] - в результаті маємо отримати [d, c, v, k]
-
-    let numToLetter = arr.filter(elem => { return elem >= 1 && elem <= 26 ? elem : false})
+let numToNumber = (arr) => {
+    let resultArr = arr.filter(elem => { return elem >= 1 && elem <= 26 ? elem : false})
     .map(elem => { return (elem + 9).toString(36) }) // також можна завернути все це у функцію
+    return resultArr;
+}
 
 // Створити функцію яка параметром отримує об’єкт (наприклад {a: 22, b: -11.35, c: 41.2, d: ‘hello’}) і повертає новий об’єкт у який містить тільки числа більші рівні 0.
 
-let sortObj = Object.fromEntries(
-    Object.entries(obj).filter(([key, val]) => { return val >= 0 ? [key, val] : false})
-);
+let sortObject (obj) => {
+    let resultObj = Object.fromEntries(
+        Object.entries(obj).filter(([key, val]) => { return val >= 0 ? [key, val] : false})
+    );
+    return resultObj;
+}
